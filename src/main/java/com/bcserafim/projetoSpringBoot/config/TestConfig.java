63b2219548b86eb4c162b3f7fcda14efbc9854de
Bruno Cesar Serafim <bcserafim@gmail.com>
2021-01-19 16:12:24 -0300

    Payment one-to-one association

--- src/main/java/com/bcserafim/projetoSpringBoot/config/TestConfig.java
+++ src/main/java/com/bcserafim/projetoSpringBoot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.bcserafim.projetoSpringBoot.entities.Category;
 import com.bcserafim.projetoSpringBoot.entities.Order;
 import com.bcserafim.projetoSpringBoot.entities.OrderItem;
+import com.bcserafim.projetoSpringBoot.entities.Payment;
 import com.bcserafim.projetoSpringBoot.entities.Product;
 import com.bcserafim.projetoSpringBoot.entities.User;
 import com.bcserafim.projetoSpringBoot.entities.enums.OrderStatus;
@@ -75,7 +76,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT ,u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		
 		
 		
@@ -88,6 +89,15 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		
+		orderRepository.saveAll(Arrays.asList(o1));
+		
+		
+		
 		
 	}
 	
