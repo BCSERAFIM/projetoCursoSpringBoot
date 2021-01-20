69d195020004a7b91c7abcc209386562413b17c4
Bruno Cesar Serafim <bcserafim@gmail.com>
2021-01-20 15:54:11 -0300

    Exception handling - delete

--- src/main/java/com/bcserafim/projetoSpringBoot/resources/exception/ResourceExceptionHandle.java
+++ src/main/java/com/bcserafim/projetoSpringBoot/resources/exception/ResourceExceptionHandle.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.bcserafim.projetoSpringBoot.services.exception.DatabaseException;
 import com.bcserafim.projetoSpringBoot.services.exception.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -25,5 +26,16 @@ public class ResourceExceptionHandle {
 		
 	}
 	
-
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
+		
+	
 }
--- src/main/java/com/bcserafim/projetoSpringBoot/services/UserService.java
+++ src/main/java/com/bcserafim/projetoSpringBoot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.bcserafim.projetoSpringBoot.entities.User;
 import com.bcserafim.projetoSpringBoot.repositories.UserRepository;
+import com.bcserafim.projetoSpringBoot.services.exception.DatabaseException;
 import com.bcserafim.projetoSpringBoot.services.exception.ResourceNotFoundException;
 
 @Service
@@ -35,7 +38,14 @@ public class UserService {
 	
 	
 	public void delete(long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 		
 	}
 
